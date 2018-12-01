dd1095ed92e11a6c2b9386e191995ad6edbd3d57
JOCELYN-LAPTOP\Jocelyn <jlepine@optiinfos.com>
2018-12-01 09:56:43 -0800

    Add project files.

---
+++ ApplocationCore/Entities/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplocationCore.Entities
+{
+    public class Player : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public Player(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
+
+    
---
+++ TennisPro/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ApplocationCore.Entities;
+
+namespace TennisPro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Orbis Tennis Pro!");
+
+            Console.WriteLine("Please enter Player 1 name:");
+            var player1Name = Console.ReadLine();
+
+            Console.WriteLine("Please enter Player 2 name:");
+            var player2Name = Console.ReadLine();
+
+            var Player1 = new Player(1, player1Name);
+            var Player2 = new Player(2, player2Name);
+            
+            Console.WriteLine("Press Enter to close the game");
+            Console.ReadLine();
+        }
+    }
+}
